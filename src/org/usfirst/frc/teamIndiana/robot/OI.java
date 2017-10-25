e4c64c79a33f19491acae8246301bc003eac44ab
Caleb Smith <karagenit@outlook.com>
2017-10-25 19:08:44 -0400

    Add Option for No AutoShift

--- src/org/usfirst/frc/teamIndiana/robot/OI.java
+++ src/org/usfirst/frc/teamIndiana/robot/OI.java
@@ -13,7 +13,7 @@ public class OI {
 
 	private Joystick driver				= new Joystick(Constants.usbDriver);
 
-	public boolean shift, score, climb;
+	public boolean score, climb;
 	public double leftY, rightY, rightX; //TODO why not have getters that force update via getRaw*()?
 	public boolean doUpshift, doDownshift;
 
@@ -22,7 +22,6 @@ public class OI {
 		leftY = -1.0 * Deadband.calcDeadbandOut(driver.getRawAxis(Constants.controllerLY));
 		rightY = -1.0 * Deadband.calcDeadbandOut(driver.getRawAxis(Constants.controllerRY));
 
-		shift = driver.getRawAxis(Constants.controllerRT)  > TRIGGER_THRESH;
 		rightX = -1.0 * Deadband.calcDeadbandOut(driver.getRawAxis(Constants.controllerRX));
 		score = driver.getRawButton(Constants.controllerLB);
 		climb = driver.getRawButton(Constants.controllerB);
@@ -39,6 +38,9 @@ public class OI {
 			} else {
 				doDownshift = Math.abs(leftY) < DOWNSHIFT_THRESH && Math.abs(rightY) < DOWNSHIFT_THRESH;
 			}
+		} else { //Don't use AutoShift
+			doUpshift = driver.getRawAxis(Constants.controllerRT)  > TRIGGER_THRESH;
+			doDownshift = !doUpshift;
 		}
 
 	}
